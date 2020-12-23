f66b2884c53e6289ed86908ec2d460920cc41ed3
Matthew Chun <matthewchun93@gmail.com>
2020-12-23 09:23:31 -0800

    added census codes

--- js/dataAnalysis.js
+++ js/dataAnalysis.js
@@ -22,15 +22,36 @@ let microAreaIds = [
 
 var censusCodeArr = [
     { name: "population", code: "B01003_001E" },
+    { name: "malePop", code: "B01001_002E" },
+    { name: "femalePop", code: "B01001_026E" },
     { name: "mAge", code: "B01002_001E" },
     { name: "mIncome", code: "B06010_001E" },
+    { name: "whitePop", code: "B02001_002E" },
+    { name: "blackPop", code: "B02001_003E" },
+    { name: "asianPop", code: "B02001_005E" },
+    { name: "nativePop", code: "B02001_004E" },
+    { name: "islanderPop", code: "B02001_006E" },
+    { name: "otherPop", code: "B02001_007E" },
+    { name: "belowHighschool", code: "B07009_002E" },
+    { name: "highshoolEquiv", code: "B07009_003E" },
+    { name: "someCollege", code: "B07009_004E" },
+    { name: "bachelors", code: "B07009_005E" },
+    { name: "graduateProf", code: "B07009_006E" },
 ];
 
+var district = document.getElementById('district-letter')
+var member = document.getElementById('member')
+var phone = document.getElementById('phone');
+var population = document.getElementById('pop');
+var income = document.getElementById('inc');
+var race = document.getElementById('race');
+var age = document.getElementById('age');
+var education = document.getElementById('edu');
+
 let codesArr = censusCodeArr.map((censusElementObj) => censusElementObj.code);
-console.log(codesArr);
 let codeArrStr = codesArr.join(",");
 
-console.log(codesArr);
+console.log(codeArrStr);
 $.getJSON(
     `https://api.census.gov/data/2019/acs/acs5?get=NAME,${codeArrStr}&for=public%20use%20microdata%20area:*&in=state:48&key=edf70f15a37d771191e6f4d62aab1871d9182206`
 ) // fetch specific character data object
@@ -39,6 +60,6 @@ $.getJSON(
         harrisCountyAreasArr = data.filter((microDataArr) => {
             return microAreaIds.includes(microDataArr.slice(-1)[0]);
         });
-
+        population.innerHTML = harrisCountyAreasArr[0][1]
         console.log(harrisCountyAreasArr);
     });
--- js/pullDistrict.js
+++ js/pullDistrict.js
@@ -21,7 +21,7 @@ let microAreaIds = [
 ];
 
 $.getJSON(
-    "https://api.census.gov/data/2019/acs/acs5?get=NAME,B01002_001E,B01003_001E&for=public%20use%20microdata%20area:*&in=state:48&key=edf70f15a37d771191e6f4d62aab1871d9182206"
+    "https://api.census.gov/data/2019/acs/acs1?get=NAME,B01002_001E,B01003_001E&for=public%20use%20microdata%20area:*&in=state:48&key=edf70f15a37d771191e6f4d62aab1871d9182206"
 ) // fetch specific character data object
     .done((data) => {
         console.log(data);
